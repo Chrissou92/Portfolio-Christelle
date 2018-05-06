96620f232e4e9a303e16053ce43628376264a9c6
chrissou <nicolchristelle@gmail.com>
2018-05-06 17:19:51 +0200

    finish home and style

--- app/src/components/Home/index.js
+++ app/src/components/Home/index.js
@@ -15,16 +15,21 @@ import React from 'react';
 
 const Home = () => (
   <div id="home" className="content">
+    <div className="content-img" />
     <section className="content-section">
+      <div className="content-profil">
+          Hello, je m'appelle Christelle, vous voici sur mon site portefolio fait en React.js.
+      </div>
 
-      <h1 className="content-title">Présentation</h1>
-      <p>
-      J'ai profité d'un congé parental récent pour me former et optimiser ma reconversion
-       professionnelle vers le métier de développeur web.
-      Après avoir suivi des cours en ligne via les MOOC, j'ai décidé d'intégrer
-       l'école O'clock pour obtenir un titre professionnel de développeur.
+
+      <h2 className="content-title">Reconversion</h2>
+      <p className="content-text">
+        J'ai profité d'un congé parental récent pour me former et optimiser ma reconversion
+        professionnelle vers le métier de développeur web.
+        Après avoir suivi des cours en ligne via les MOOC, j'ai décidé d'intégrer
+        l'école O'clock pour obtenir un titre professionnel de développeur.
+        Après 5 mois de formation intensive, je me lance pour une nouvelle aventure !
       </p>
-
     </section>
   </div>
 );
