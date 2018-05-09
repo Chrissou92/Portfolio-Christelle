b7afe6cc36a9b44fdffa832f45a8d937e17d785a
Chrissou <nicolchristelle@gmail.com>
2018-05-09 11:40:02 +0200

    oubli ctrl s :D

--- app/src/components/Contact/index.js
+++ app/src/components/Contact/index.js
@@ -13,8 +13,8 @@ const Contact = () => (
   <div id="contact">
     <p className="contact">Contact</p>
     <p className="link">Visit my <a href="https://twitter.com/nicolchristelle" target="_blank" className="external">Twitter</a></p>
-    <p  className="link">Visit my <a href="https://www.linkedin.com/in/christelle-nicol-%F0%9F%91%A9%E2%80%8D%F0%9F%92%BB-2aba93147/" className="external">Linkedin</a></p>
-    <p  className="link">Visit my <a href="https://github.com/Chrissou92" className="external">GitHub</a></p>
+    <p  className="link">Visit my <a href="https://www.linkedin.com/in/christelle-nicol-%F0%9F%91%A9%E2%80%8D%F0%9F%92%BB-2aba93147/" target="_blank"  className="external">Linkedin</a></p>
+    <p  className="link">Visit my <a href="https://github.com/Chrissou92" target="_blank"  className="external">GitHub</a></p>
   </div>
 );
 /*
